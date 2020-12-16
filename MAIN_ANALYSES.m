74c6e8d2b5e141cf156372720044096dc244d4bc
samlaventure <samuel.laventure@gmail.com>
2020-12-16 20:18:19 +0100

    first commit - Saving figures function

--- MAIN_ANALYSES.m
+++ MAIN_ANALYSES.m
@@ -1,10 +1,13 @@
 %%   MAIN ANALYSES
 clear all
 
-
 %% Global Parameters
-expe = 'StimMFBWake';
-mice_num = [124];
+expe = 'StimMFBWake';   % experiment name
+mice_num = [117];       % mice ID #
+dirPath = [dropbox '/DataSL/StimMFBWake/ripples/Sleep/' date '/'];  % folder where to save
+
+
+
 
 %% Behavior
 BehaviorERC_SL_8trials(expe,mice_num);
@@ -13,6 +16,14 @@ BehaviorERC_SL_8trials(expe,mice_num);
 %% Ripples
 % during sleep
 Ripples_sleep(mice_num);
+
 % ripples during task
 RipplesDuringTask_SL(expe,mice_num);
+
+% compare pre/post ripples
+H = compRipStages(expe, [mice_num]);
+    figName = ['GlobalAnalyses_ripples_' num2str(mice_num)];
+    saveF(H.global,figName,dirPath,'sformat',{'dpng'},'res',300,'savfig',0)
+    figName = ['DiffAnalyses_ripples_' num2str(mice_num)];
+    saveF(H.diff,figName,dirPath)
 
