d25cb2e5e8eb766f853f5440251afc3cfdd88376
samlaventure <samuel.laventure@gmail.com>
2021-04-10 20:48:06 +0200

    Create sleep_prevspost_bysess_quick.m

---
+++ Quick_scripts/sleep_prevspost_bysess_quick.m
@@ -0,0 +1,24 @@
+%%   MAIN ANALYSES
+clear all
+expe = 'StimMFBWake'; 
+expe = 'UMazePAG'; 
+
+mice_num = [861];
+
+ss = 'accelero';
+
+thresh = [2 1];
+
+% path
+dirPath_sMFBW_sleep = [dropbox 'DataSL/' expe '/Sleep/' date '/'];  % folder where to save
+
+% detect deltas by session
+s_launchDeltaDetect(expe,mice_num,ss,thresh)
+
+% Need to run s_launchDeltaDetect first
+Hsleep = sam_sleep_prevspost_bysess(expe,mice_num,'stim',0); % plot using delta detection restricted to sleep sessions
+    for i=1:length(mice_num)
+        figName = ['M' num2str(mice_num(i)) '_sleeparch_prepost'];
+        saveF(Hsleep.SleepArch_single{i},figName,dirPath_sMFBW_sleep,'sformat',{'dpng'},'res',300,'savfig',0)
+        disp(['M' num2str(mice_num(i)) ' saved']);
+    end
