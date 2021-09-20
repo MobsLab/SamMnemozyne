64c2bf1f80ded2715deda981a4902ee13f4d34b4
samlaventure <samuel.laventure@gmail.com>
2021-09-20 14:20:01 +0200

    Update MAIN_ANALYSES.m
    specifies expe number (multi-expe by mouse)

--- MAIN_ANALYSES.m
+++ MAIN_ANALYSES.m
@@ -2,64 +2,80 @@
 clear all
 
 %% Global Parameters
-expe{1} = 'StimMFBWake';  
-mice_num{1} = 882;
+% expe{1} = 'UMazePAG';
+expe{1} = 'StimMFBWake';
+expe{1} = 'Novel';
+
+mice_num{1} = 1199;
+numexpe{1} = 2;
+
+% all MFB
+mice_num{1} = [882 941 1117 1161 1162 1168 1199 1199 1223 1228];  % mice ID #
+numexpe{1} = [1 1 1 1 1 1 1 2 1 1];
+% MFB with spindles and deltas
+mice_num{1} = [882 1117 1161 1162 1168 1199 1199 1228];  % mice ID #
+numexpe{1} = [1 1 1 1 1 1 2 1];
 
 expe{2} = 'UMazePAG';
-mice_num{1} = [882 941 1117 1161 1162 1168];       % mice ID #
-mice_num{2} = [798 828 861 882 905 906 911 912 977 994 1117 1124 1161 1162 1168]; 
+mice_num{2} = [797 798 828 861 882 905 912 977 994 1117 1124 1161 1162 1168 1182 1186 1199]; 
 % mice_num{2} = [797 798 828 861 882 905 906 911 912 977 994 1117 1124 1161 1162 1168]; 
 same_id = [1 5; 3 12; 4 14; 5 15; 6 16];
 
 expe = 'StimMFBWake'; 
-mice_num = [882]
+mice_num = [882];
 mice_num =  [882 941 117 161 162 168];  
 mice_num =  [863 934 913 882 941 081 117 124 161 162 168 182];  
-
 
 expe = 'UMazePAG';
 mice_num = [798 828 861 882 905 906 911 912 977 994 1117 1124 1161 1162 1168];
 
-
-
 % specific params
-limtrials = 0; % limited pre- post-tests trials
+limtrials = 1; % limited pre- post-tests trials
 recompute = 0; % recalculate (way slower but necesssary if change in past code)
 
 %% Saving folder paths
+iexp=1;
 % behavior
-dirPath_behav = [dropbox '/DataSL/' expe '/Behavior/' date '/'];
+dirPath_behav = [dropbox '/DataSL/' expe{iexp} '/Behavior/'];
 % ripples
-dirPath_sMFBW_rip = [dropbox 'DataSL/' expe '/ripples/Sleep/' date '/'];  
+dirPath_sMFBW_rip = [dropbox 'DataSL/' expe{iexp} '/ripples/Sleep/' date '/'];  
 % spindles
-dirPath_sMFBW_spin = [dropbox 'DataSL/' expe '/spindles/Sleep/' date '/'];
+dirPath_sMFBW_spin = [dropbox 'DataSL/' expe{iexp} '/spindles/Sleep/' date '/'];
 % sleep
-dirPath_sMFBW_sleep = [dropbox 'DataSL/' expe '/Sleep/' date '/'];  % folder where to save
+dirPath_sMFBW_sleep = [dropbox 'DataSL/' expe{iexp} '/Sleep/' date '/'];  % folder where to save
 % sleep arch comp mfb vs pag
 dirPath_sleepArch_mfbvspag = [dropbox 'DataSL/MFBvsPAG/Sleep/' date '/']; 
 % mfb vs pag speed analyses
 dirPath_speed_mfbvspag = [dropbox 'DataSL/MFBvsPAG/Speed/' date '/']; 
 
-
 %% Behavior
-[figH_ind figH] = BehaviorERC_SL_v3(expe,mice_num,limtrials,recompute);
-    for i=1:length(mice_num)
+iexp=1;
+[figH_ind figH] = BehaviorERC_SL_v3(expe{1},mice_num{1},numexpe{1},limtrials,recompute);
+    for i=1:length(mice_num{1})
         % individual
         if isfield(figH_ind,'dirspeed')
-            figName = ['Behav_speedDirection__limt' num2str(limtrials) '_'  num2str(mice_num(i))];
-            saveF(figH_ind.dirspeed{i},figName,dirPath_behav,'sformat',{'dpng'},'res',300,'savfig',0)
+            figName = [num2str(mice_num{iexp}(i)) '_Behav_speedDirection_exp' ...
+                num2str(numexpe{1}(i)) '_limt' num2str(limtrials) '_' date];
+            saveF(figH_ind.dirspeed{i},figName,[dirPath_behav  num2str(mice_num{iexp}(i)) '/'], ...
+                'sformat',{'dpng'},'res',300,'savfig',0)
         end
         if isfield(figH_ind,'trajdyn')
-            figName = ['Behav_pertrial_zonedyna_'  num2str(mice_num(i))];
-            saveF(figH_ind.trajdyn{i},figName,dirPath_behav,'sformat',{'dpng'},'res',300,'savfig',0)
+            figName = [num2str(mice_num{iexp}(i)) '_Behav_pertrial_zonedyna_exp' ...
+                num2str(numexpe{1}(i)) '_limt' num2str(limtrials) '_' date];
+            saveF(figH_ind.trajdyn{i},figName,[dirPath_behav  num2str(mice_num{iexp}(i)) '/'], ...
+                'sformat',{'dpng'},'res',300,'savfig',0)
         end
         if isfield(figH_ind,'firstentry')
-            figName = ['Behav_latency1stentry_'  num2str(mice_num(i))];
-            saveF(figH_ind.firstentry{i},figName,dirPath_behav,'sformat',{'dpng'},'res',300,'savfig',0)
+            figName = [num2str(mice_num{iexp}(i)) '_Behav_latency1stentry_exp' ...
+                num2str(numexpe{1}(i)) '_limt' num2str(limtrials) '_' date];
+            saveF(figH_ind.firstentry{i},figName,[dirPath_behav  num2str(mice_num{iexp}(i)) '/'], ...
+                'sformat',{'dpng'},'res',300,'savfig',0)
         end
         if isfield(figH_ind,'trajoccup')
-            figName = ['Behav_Trajectories_per_trial_'  num2str(mice_num(i))];
-            saveF(figH_ind.trajoccup{i},figName,dirPath_behav,'sformat',{'dpng'},'res',300,'savfig',0)
+            figName = [num2str(mice_num{iexp}(i)) '_Behav_Trajectories_per_exp' ...
+                num2str(numexpe{1}(i)) '_trial_limt' num2str(limtrials) '_' date];
+            saveF(figH_ind.trajoccup{i},figName,[dirPath_behav  num2str(mice_num{iexp}(i)) '/'], ...
+                'sformat',{'dpng'},'res',300,'savfig',0)
         end
     end
     % global
@@ -68,30 +84,30 @@ dirPath_speed_mfbvspag = [dropbox 'DataSL/MFBvsPAG/Speed/' date '/'];
         saveF(figH.globalspeed,figName,dirPath_behav,'sformat',{'dpng'},'res',300,'savfig',0)
     end
     if isfield(figH,'globalstats')
-        figName = 'general_basic_stats';
+        figName = ['general_basic_stats_limt' num2str(limtrials)];
         saveF(figH.globalstats,figName,dirPath_behav,'sformat',{'dpng'},'res',300,'savfig',0)
     end
     if isfield(figH,'heatmaps')
-        figName = 'Heatmaps_precondpost';
+        figName = ['Heatmaps_precondpost_limt' num2str(limtrials)];
         saveF(figH.heatmaps,figName,dirPath_behav,'sformat',{'dpng'},'res',300,'savfig',0)
     end
     if isfield(figH,'finalfig')
-        figName = 'finalfig';
+        figName = ['finalfig_limt' num2str(limtrials)];
         saveF(figH.finalfig,figName,dirPath_behav,'sformat',{'dpng'},'res',300,'savfig',0)
     end
     if isfield(figH,'heatstat')
-        figName = 'Heatstat';
+        figName = ['Heatstat_limt' num2str(limtrials)];
         saveF(figH.heatstat,figName,dirPath_behav,'sformat',{'dpng'},'res',300,'savfig',0)
     end
 
 %% Sleep
 % Hsleep = sam_sleep_prevspost(expe,mice_num,'stim',0);
 % Need to run s_launchDeltaDetect first
-Hsleep = sam_sleep_prevspost_bysess(expe,mice_num,'stim',0); % plot using delta detection restricted to sleep sessions
-    for i=1:length(mice_num)
-        figName = ['M' num2str(mice_num(i)) '_sleeparch_prepost'];
+Hsleep = sam_sleep_prevspost_bysess(expe,mice_num{1},numexpe{1},'stim',0); % plot using delta detection restricted to sleep sessions
+    for i=1:length(mice_num{1})
+        figName = ['M' num2str(mice_num{1}(i)) '_exp' num2str(numexpe{1}) '_sleeparch_prepost'];
         saveF(Hsleep.SleepArch_single{i},figName,dirPath_sMFBW_sleep,'sformat',{'dpng'},'res',300,'savfig',0)
-        disp(['M' num2str(mice_num(i)) ' saved']);
+        disp(['M' num2str(mice_num{1}(i)) ' saved']);
     end
     
 % SLEEP ARCHITECTURE comparison between MFB vs PAG 
@@ -107,28 +123,28 @@ Harch = compSleepArch_exp(expe, mice_num);
     saveF(Harch.measures,figName,dirPath_sleepArch_mfbvspag,'sformat',{'dpng'},'res',300,'savfig',0)
     disp(['Figures saved']);    
 
-% %% Ripples
-% % during sleep
-% Ripples_sleep(expe,mice_num);
-% 
-% % ripples during task
-% RipplesDuringTask_SL(expe,mice_num);
-% 
+%% Ripples
+% during sleep
+Ripples_sleep(expe,mice_num);
+
+% ripples during task
+RipplesDuringTask_SL(expe,mice_num{1},numexpe{1});
+
 % compare pre/post ripples
-% H = compRipStages(expe, [mice_num]);
-%     figName = ['GlobalAnalyses_ripples_' num2str(mice_num)];
-%     saveF(H.global,figName,dirPath_sMFBW_rip,'sformat',{'dpng'},'res',300,'savfig',0)
-%     figName = ['DiffAnalyses_ripples_' num2str(mice_num)];
-%     saveF(H.diff,figName,dirPath_sMFBW_rip)
-%     
-% %% SPINDLES
-% % compare pre/post spindles
-% H = compSpindleStages(expe, [mice_num]);
-%     figName = ['GlobalAnalyses_spindles_' num2str(mice_num)];
-%     saveF(H.global,figName,dirPath_sMFBW_spin,'sformat',{'dpng'},'res',300,'savfig',0)
-%     figName = ['DiffAnalyses_spindles_' num2str(mice_num)];
-%     saveF(H.diff,figName,dirPath_sMFBW_spin)
-%     
+H = compRipStages(expe, [mice_num{1}], numexpe{1});
+    figName = ['GlobalAnalyses_ripples_' num2str(mice_num{1})];
+    saveF(H.global,figName,dirPath_sMFBW_rip,'sformat',{'dpng'},'res',300,'savfig',0)
+    figName = ['DiffAnalyses_ripples_' num2str(mice_num{1})];
+    saveF(H.diff,figName,dirPath_sMFBW_rip)
+    
+%% SPINDLES
+% compare pre/post spindles
+H = compSpindleStages(expe, [mice_num{1}], numexpe{1});
+    figName = ['GlobalAnalyses_spindles_' num2str(mice_num{1})];
+    saveF(H.global,figName,dirPath_sMFBW_spin,'sformat',{'dpng'},'res',300,'savfig',0)
+    figName = ['DiffAnalyses_spindles_' num2str(mice_num{1})];
+    saveF(H.diff,figName,dirPath_sMFBW_spin)
+    
 
 H = PAGvsMFB_Speed(expe,mice_num)
     figName = ['Speed_MFBvsPAG'];
